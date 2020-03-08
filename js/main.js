ac41dfb00ef65cf2eedd9500532ad85a074683e4
Christine <christine-xia@hotmail.com>
2020-03-07 21:24:59 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ $(document).mousemove(function(e){
 	$('.cursor').css({'top': e.clientY + 2, 'left': e.clientX + 2})
 })
 
+$(document).one('mousemove', function(){
+	$('img.cursor.black').css({'display':'block'});
+})
+
+
 // CURSOR HOVER 
 
 $('img.cursor.white').css({'display':'none'});
@@ -16,5 +21,35 @@ $('a').hover(
 	function(){
 		$('img.cursor.black').css({'display':'block'});
 		$('img.cursor.white').css({'display':'none'});
+	}
+)
+
+
+// ICON HOVER (GIF/PNG)
+
+$('.title').hover(
+	function(){
+		$('.icon.gif').css({'display':'inline'})
+		$('.icon.still').css({'display':'none'})
+	},
+	function(){
+		$('.icon.gif').css({'display':'none'})
+		$('.icon.still').css({'display':'inline'})
+	}
+)
+
+
+// HOVER APPEAR IMAGE + IMAGE MOVE WITH CURSOR
+
+$(document).mousemove(function(e){
+	$('.hoverimg').css({'top': e.clientY + 27, 'left': e.clientX + 27})
+})
+
+$('#me').hover(
+	function(){
+		$('.hoverimg').css({'display':'block'})
+	},
+	function(){
+		$('.hoverimg').css({'display':'none'})
 	}
 )
